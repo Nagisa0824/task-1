ea4a8771f9faa975036b25b7c1c87bead4e2c5d4
Nagisa0824 <yuzutea0824@gmail.com>
2021-05-20 16:09:01 +0000

    Initial Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+  validates :genre, presence: true
+  enum genre:{
+        漫画:0,雑誌:1,小説:2,未定:3
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_015140) do
+ActiveRecord::Schema.define(version: 2021_05_20_152336) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.integer "genre"
   end
 
 end
